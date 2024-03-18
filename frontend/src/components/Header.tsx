8d8dfe69ceaa994f99f894747dd8ea3c28f38f9b
tam0w <tammu1000@gmail.com>
2024-03-18 17:18:55 +0530

    commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,54 @@
-import { AppBar, Toolbar } from '@mui/material'
-import React from 'react'
-import Logo from './shared/Logo'
+import { AppBar, Toolbar } from "@mui/material";
+import React from "react";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
-    <AppBar sx={{bgcolor:"transparent ", position:"static", boxShadow:"none"}}>
+    <AppBar
+      sx={{ bgcolor: "transparent ", position: "static", boxShadow: "none" }}
+    >
+       <Toolbar sx={{ display: "flex" }}>
       <Logo/>
-      <Toolbar sx={{display:"flex"}}></Toolbar>
+      <div >
+        {auth?.isLoggedIn ? (
+          <>
+            <NavigationLink
+              bg="#00fffc"
+              to="/chat"
+              text="Go to chat"
+              textColor="black"
+            />
+            <NavigationLink
+              bg="#51538f"
+              textColor="white"
+              to="/"
+              text="logout"
+              onClick={auth.logout}
+            />
+          </>
+        ) : (
+          <>
+            <NavigationLink
+              bg="#00fffc"
+              to="/login"
+              text="login"
+              textColor="black"
+            />
+            <NavigationLink
+              bg="#51538f"
+              textColor="white"
+              to="/signup"
+              text="signup"
+            />
+          </>
+        )}
+      </div>
+     </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
